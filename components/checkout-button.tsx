77569640cdfab1b3f9a8e59391573f9c659a118e
Ruhaim <ruhaim860@gmail.com>
2023-11-02 12:53:13 +0530

    Add pricing page and pricing card

---
+++ components/checkout-button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CheckoutButton = () => {
+  return <div>CheckoutButton</div>;
+};
--- components/create-chat-button.tsx
+++ components/create-chat-button.tsx
@@ -3,10 +3,17 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { MessageSquarePlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const CreateChatButton = () => {
+  const router = useRouter();
+
+  const createNewChat = async () => {
+    // logic here
+    router.push("/chat/abc");
+  };
   return (
-    <Button variant={"ghost"}>
+    <Button onClick={createNewChat} variant={"ghost"}>
       <MessageSquarePlusIcon />
     </Button>
   );
